3d715a763261780e76ef2c52c07e716d72efd9b9
Sarah Manning <sarahnmanning@gmail.com>
2018-07-05 19:28:54 -0400

    Added the ability to select the amount for added or subtracting

--- src/Counter.js
+++ src/Counter.js
@@ -4,21 +4,28 @@ class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          count:0
+          count: 0,
+          amount: 1
         };
       }
     
     increment() {
       this.setState({
-        count: this.state.count + 1
+        count: this.state.count + this.state.amount
       });
     }
   
     decrement() {
       this.setState({
-        count: this.state.count - 1
+        count: this.state.count - this.state.amount
       })
     }  
+
+    changeAmount(evt) {
+      this.setState({
+        amount: parseInt(evt.target.value, 10)
+      })
+    }
 
     render () {
         return (
@@ -26,6 +33,13 @@ class Counter extends Component {
                 <button onClick={this.increment.bind(this)}>+</button>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.decrement.bind(this)}>-</button>
+                <input type='number' 
+                          min='1' 
+                          max='100' 
+                          step='1' 
+                          placeholder='Amount to count...' 
+                          value={this.state.amount}
+                          onChange={this.changeAmount.bind(this)}/>
             </div>
                 )
     }
