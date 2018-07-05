d9edde7b663848f9eaac57ba9019bcd6a2191cce
Sarah Manning <sarahnmanning@gmail.com>
2018-07-05 18:36:05 -0400

    moved increments and decrement from app to individual counter components.

--- src/App.js
+++ src/App.js
@@ -3,35 +3,14 @@ import './App.css';
 import Counter from './Counter';
 
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count:0
-      };
-    }
-  
-  increment() {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
-  decrement() {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }  
-
+    
   render() {
     return (
       <div>
         <Counter />
         <Counter />
-        <button onClick={this.increment.bind(this)}>+</button>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement.bind(this)}>-</button>
       </div>
-    );
+            );
   }
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count:0
+        };
+      }
+    
+    increment() {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }
+  
+    decrement() {
+      this.setState({
+        count: this.state.count - 1
+      })
+    }  
+
     render () {
         return (
             <div>
-                <button>+</button>
-                <h1>0</h1>
-                <button>-</button>
+                <button onClick={this.increment.bind(this)}>+</button>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.decrement.bind(this)}>-</button>
             </div>
                 )
     }
